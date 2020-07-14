577f314d2e0a6850cf39f23e3a87182c26b4e670
arhaanb <arhaanb@gmail.com>
2020-07-14 16:46:19 +0530

    hode footer img on click

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 $(".about").click(function () {
-    $('#about').slideToggle();
-    $('#credits').hide();
-    $('.about').toggleClass('hi');
-    $('.credits').removeClass('hi');
+	$('#about').slideToggle();
+	$('#credits').hide();
+	$('.about').toggleClass('hi');
+	$('.credits').removeClass('hi');
+	$('footer').toggleClass('foot');
 });
 
 $(".credits").click(function () {
-    $('#credits').slideToggle();
-    $('#about').hide();
-    $('.credits').toggleClass('hi');
-    $('.about').removeClass('hi');
+	$('#credits').slideToggle();
+	$('#about').hide();
+	$('.credits').toggleClass('hi');
+	$('.about').removeClass('hi');
+	$('footer').toggleClass('foot');
 });
 
 
 $(document).on('click', function (event) {
-    $('#about, #credits').fadeOut();
-    $('.about, .credits').removeClass('hi');
+	$('#about, #credits').fadeOut();
+	$('.about, .credits').removeClass('hi');
+	$('footer').removeClass('foot');
 });
 
 $('.reveal').on('click', function (event) {
-    event.stopPropagation();
+	event.stopPropagation();
 });
 $('.link').on('click', function (event) {
-    event.stopPropagation();
+	event.stopPropagation();
 });
